434c7fe7c8cb4a852a308fe8b24e6e607a243f52
Matthias Pabel <matthias.pabel@googlemail.com>
2017-08-27 17:21:12 +0200

    refactor

--- game.py
+++ game.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
 # -*- coding: <utf-8> -*-
-import numpy as np
-import pygame
 import sys
+
+import pygame
 from pygame.locals import QUIT
 
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREY = (50, 50, 50)
+from entity import Player
 
 
 class Game:
@@ -49,45 +47,3 @@ class Game:
         self.screen.blit(self.background, (0, 0))
         self.group.draw(self.screen)
         pygame.display.flip()
-
-
-class Entity(pygame.sprite.Sprite):
-    """
-    Base Class for all Entities
-
-    self.image and self.rect should be overwritten
-    """
-
-    def __init__(self, x: int, y: int) -> None:
-        super().__init__()
-        self.image = pygame.Surface([10, 10])
-        self.image.fill(GREY)
-        self.rect = self.image.get_rect()
-        self.velocity = np.array((0, 0))
-
-    @property
-    def pos(self) -> np.array:
-        return np.array((self.rect.x, self.rect.y))
-
-    @pos.setter
-    def pos(self, pos: np.array):
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
-
-    def update(self) -> None:
-        raise NotImplementedError("abstract Method")
-
-
-class Player(Entity):
-    VELO_MAX = (10, 10)
-    VELO_MIN = (0, 0)
-
-    def __init__(self, x: int, y: int) -> None:
-        super().__init__(x, y)
-        self.image = pygame.Surface([50, 50])
-        self.image.fill(RED)
-        self.rect = self.image.get_rect()
-        self.pos = (x, y)
-
-    def update(self):
-        pass
--- launcher.py
+++ launcher.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: <utf-8> -*-
 
-from Game import Game
+from game import Game
 
 
 def launch() -> None:
