a391dbbfde63a38cbbaa69030fa4b9683aeca6b1
Matthias Pabel <matthias.pabel@googlemail.com>
2017-08-27 18:40:45 +0200

    moving

--- game.py
+++ game.py
@@ -3,7 +3,7 @@
 import sys
 
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import QUIT, KEYDOWN, K_UP, K_DOWN, K_LEFT, K_RIGHT, KEYUP
 
 from colors import BLACK
 from entity import Player
@@ -13,6 +13,8 @@ class Game:
     def __init__(self) -> None:
         pygame.init()
         self.screen = self.__init_screen()
+        pygame.event.set_allowed(None)
+        pygame.event.set_allowed((KEYDOWN, KEYUP, QUIT))
         self.player = Player(250, 250)
         self.players = pygame.sprite.RenderPlain(self.player)
         self.destroits = pygame.sprite.Group()
@@ -43,18 +45,28 @@ class Game:
             self.render()
 
     def process_input(self) -> None:
+        pygame.event.pump()
         for event in pygame.event.get():
-            print(event)
             if event.type == QUIT:
                 sys.exit(0)
-            if event.type == pygame.locals.KEYDOWN and event.key == pygame.locals.K_UP:
-                self.player.update_velocity(0, -1)
-            if event.type == pygame.locals.KEYDOWN and event.key == pygame.locals.K_DOWN:
-                self.player.update_velocity(0, 1)
-            if event.type == pygame.locals.KEYDOWN and event.key == pygame.locals.K_LEFT:
-                self.player.update_velocity(-1, 0)
-            if event.type == pygame.locals.KEYDOWN and event.key == pygame.locals.K_RIGHT:
-                self.player.update_velocity(1, 0)
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    self.player.update_velocity(0, -1)
+                if event.key == K_DOWN:
+                    self.player.update_velocity(0, 1)
+                if event.key == K_LEFT:
+                    self.player.update_velocity(-1, 0)
+                if event.key == K_RIGHT:
+                    self.player.update_velocity(1, 0)
+            if event.type == KEYUP:
+                if event.key == K_UP:
+                    self.player.update_velocity(0, 1)
+                if event.key == K_DOWN:
+                    self.player.update_velocity(0, -1)
+                if event.key == K_LEFT:
+                    self.player.update_velocity(1, 0)
+                if event.key == K_RIGHT:
+                    self.player.update_velocity(-1, 0)
 
     def render(self) -> None:
         self.screen.blit(self.background, (0, 0))
